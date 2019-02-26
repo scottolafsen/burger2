437a942c395d2c0b3761fab73b7b669e6d73d9c9
Scott <scott.olafsen@gmail.com>
2019-02-25 20:42:52 -0700

    first

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('mushroom', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('buffalo', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('swiss', false);
